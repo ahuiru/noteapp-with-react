d79b8cd1886f1e64981cf75d492fcf9993b9e177
ahuiru <zhuangerlangyou@gmail.com>
2022-10-04 23:55:55 +0900

    sidebar css

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,8 +3,21 @@ import './Sidebar.css';
 
 export const Sidebar = () => {
   return (
-    <div>
-      Sidebar
+    <div className='app-sidebar'>
+      <div className='app-sidebar-header'>
+        <h1>ノート</h1>
+        <button>追加</button>
+      </div>
+      <div className='app-sidebar-notes'>
+        <div className='app-sidebar-note'>
+          <div className='app-sidebar-note-title'>
+            <strong>タイトル</strong>
+            <button>削除</button>
+          </div>
+          <p>ノートの内容</p>
+          <small>最後の編集日: ++ / ++</small>
+        </div>
+      </div>
     </div>
 
   )
