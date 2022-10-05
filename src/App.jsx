9a40287c93a96624b981c90a6a4c431f8cac07ab
ahuiru <zhuangerlangyou@gmail.com>
2022-10-05 13:21:18 +0900

    sidebar highlight

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import uuid from 'react-uuid';
 
 function App() {
   const [notes, setNotes] = useState([]);
+  const [activeNote, setActiveNote] = useState(false);
 
   const onAddNote = () => {
     const newNote = {
@@ -25,7 +26,13 @@ function App() {
 
   return (
     <div className="App">
-      <Sidebar onAddNote={onAddNote} notes={notes} onDeleteNote={onDeleteNote}/>
+      <Sidebar
+        onAddNote={onAddNote}
+        notes={notes}
+        onDeleteNote={onDeleteNote}
+        activeNote={activeNote}
+        setActiveNote={setActiveNote}
+      />
       <Main />
     </div>
   )
